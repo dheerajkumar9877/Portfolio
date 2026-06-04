4a4dd6ec5fd48b9d1d7025c6fc7fe269862fa72c
Dheerajkumar <dheerajkumar9877k@gmail.com>
2026-06-04 21:54:01 +0530

    Image feature

--- logic.js
+++ logic.js
@@ -4,3 +4,14 @@ function scrollToSection(sectionId) {
         element.scrollIntoView({ behavior: 'smooth' });
     }
 }
+function zoomImage(img) {
+    const modal = document.getElementById("imageModal");
+    const zoomedImage = document.getElementById("zoomedImage");
+
+    zoomedImage.src = img.src;
+    modal.style.display = "flex"; 
+}
+
+function closeImage(){
+    document.getElementById("imageModal").style.display = "none";
+}
